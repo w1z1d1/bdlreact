60d2729fa3af85968170e1740f033452a1e428fd
w1z1d1 <1623051380@qq.com>
2020-07-09 09:37:03 +0800

    1.0

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import {HashRouter, Route, Switch} from "react-router-dom";
 import Login from "./pages/login";
-
+import Home from "./pages/home";
+import Register from "./pages/register";
+import Buy from "./pages/buy";
 function App() {
     return (
         <HashRouter>
             <div>
                 <Switch>
-                    <Route exact strict path="/" component={Login}/>
+                    <Route path='/login' component={Login}/>
+                    <Route exact strict path="/" component={Home} />
+                    <Route path='/register' component={Register}/>
+                    <Route path='/buy' component={Buy}/>
                 </Switch>
             </div>
         </HashRouter>
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,41 @@
 import React from "react";
-import { Button } from 'antd';
-
+import {Button, Menu} from 'antd';
+import 'antd/dist/antd.css';
+import './loginstyle.css';
 export default class Login extends React.Component {
-
+    toBuyPage=()=>{
+        this.props.history.push('/buy')
+    }
     render() {
-        return (
-            <div>
-                Hello!
-                <Button type="primary">Primary Button</Button>
-            </div>
-        )
+        const a1= {
+            background:' yellow',
+        }
+        const bg={
+            display:'inline-block',
+            height: '706px',
+            width:'1250px',
+            background: `url(${require("C:\\Users\\16230\\Desktop\\bdlreact\\src\\pages\\login\\新品4.png")})`
+        }
+        const bg1= {
+            display: 'inline-block',
+            height: '706px',
+            width: '1250px',
+            background: `url(${require("C:\\Users\\16230\\Desktop\\bdlreact\\src\\pages\\login\\新品图.png")})`
+        }
+        const a= {
+            frontsize: 100,
+            color: '#FF0000',
+        }
+        return <div>
+            <p><span style={a1}></span></p>
+            <p><span style={bg}></span></p>
+            <p><span style={bg1}></span></p>
+            <Button type="link" onClick={this.toBuyPage} style={{left:'250px'}}>会员登录</Button>
+            <Button type="link" onClick={this.toBuyPage} style={{left:'250px'}}>注册</Button>
+            <h1 style = {a}>菜鸟教程</h1>
+            <h2>菜鸟教程</h2>
+            <h3>欢迎学习 React</h3>
+        </div>
 
     }
 }
